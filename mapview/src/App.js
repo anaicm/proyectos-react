8ffade4bfdc1f2ef2306c12d33b63537a581a5a5
Anaicm <anaicm1@gmail.com>
2024-02-15 09:01:22 +0100

    vista

--- mapview/src/App.js
+++ mapview/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+//import Map from './components/Maps/Map';
 import Icons from './components/Maps/Icons';
 //import MapIconsPersonalizados from './components/Maps/MapIconsPersonalizados';
 //import MapCircunferenciaTerremotos from'./components/Maps/MapCircunferenciaTerremotos';
 //import MapOriginal from './components/Maps/MapOriginal';
+
 
 
 function App() {
