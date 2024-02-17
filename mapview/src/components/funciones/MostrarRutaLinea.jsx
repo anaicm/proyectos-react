9b04fc57ccb1eb4aef35bd74c47688d25f2cbd4d
Anaicm <anaicm1@gmail.com>
2024-02-17 11:45:25 +0100

    Funcion para realizar la capa de ruta en línea

---
+++ mapview/src/components/funciones/MostrarRutaLinea.jsx
@@ -0,0 +1,17 @@
+const addRouteLayer = (map) => {
+    map.addLayer({
+      id: "route",
+      type: "line",
+      source: "route",
+      layout: {
+        "line-join": "round",
+        "line-cap": "round",
+      },
+      paint: {
+        "line-color": "#888",
+        "line-width": 8,
+      },
+    });
+  };
+  
+  export default addRouteLayer;
