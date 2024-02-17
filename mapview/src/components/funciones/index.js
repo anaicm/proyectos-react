f00e7d378f243878afba1cd99465d5f2cde889a4
Anaicm <anaicm1@gmail.com>
2024-02-17 11:44:59 +0100

    añadir en el archivo barrir la funcion para la capa del mapa que realiza una ruta en línea

--- mapview/src/components/funciones/index.js
+++ mapview/src/components/funciones/index.js
@@ -4,3 +4,4 @@ export {default as CapaIconos} from './capaIconos.jsx';
 export {default as CapaTerremotos} from './capaTerremotos.jsx';
 export {default as CapaTerremotosNoAgrupados} from './capaTerremotosNoAgrupados.jsx';
 export {default as CapaTerremotosRecuento} from './capaTerremotosRecuento.jsx';
+export {default as addRouteLayer} from './MostrarRutaLinea.jsx';
